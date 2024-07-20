c3b4c394af307a59795d5255f079b4f2c1c047cb
teuku munawar <munawartmk@gmail.com>
2024-07-21 03:30:49 +0700

    Initialize gateway service

---
+++ services/gateway-service/src/test/java/com/ecommer/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommer.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
